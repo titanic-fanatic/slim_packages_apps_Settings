0072b6ad9dfe9ea518f675bd5ca1fd5f0e4c6390
Tylor Janzen <tylor.janzen@gmail.com>
2014-08-17 18:13:30 -0600

    Settings: Add Omni Performance Control

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_JAVA_LIBRARIES := bouncycastle conscrypt telephony-common
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13 jsr305
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13 jsr305 libGoogleAnalyticsV2
 
 LOCAL_MODULE_TAGS := optional
 
@@ -17,12 +17,23 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
-LOCAL_AAPT_FLAGS := --extra-packages com.koushikdutta.superuser:com.koushikdutta.widgets --auto-add-overlay
+LOCAL_AAPT_FLAGS += --auto-add-overlay \
+                    --extra-packages com.koushikdutta.superuser \
+                    --extra-packages com.koushikdutta.widgets \
+                    --extra-packages com.brewcrewfoo.performance
 
-LOCAL_SRC_FILES += $(call all-java-files-under,../../../external/koush/Superuser/Superuser/src) $(call all-java-files-under,../../../external/koush/Widgets/Widgets/src)
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res $(LOCAL_PATH)/../../../external/koush/Widgets/Widgets/res $(LOCAL_PATH)/../../../external/koush/Superuser/Superuser/res
+LOCAL_SRC_FILES += $(call all-java-files-under, ../../../external/koush/Superuser/Superuser/src) \
+                   $(call all-java-files-under, ../../../external/koush/Widgets/Widgets/src) \
+                   $(call all-java-files-under, ../PerformanceControl/src)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+                      $(LOCAL_PATH)/../../../external/koush/Widgets/Widgets/res \
+                      $(LOCAL_PATH)/../../../external/koush/Superuser/Superuser/res \
+                      $(LOCAL_PATH)/../PerformanceControl/res
+
+LOCAL_ASSET_DIR += $(LOCAL_PATH)/../PerformanceControl/assets
 
 include $(BUILD_PACKAGE)
 
-# Use the folloing include to make our test apk.
+# Use the following include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
